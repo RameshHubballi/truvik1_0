a75f364d17f244d577a87b3ad1958a9d3126fc57
rameshhubballi <rhubballi4@gmail.com>
2026-05-28 09:35:45 +0000

    added background images

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,8 +7,57 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     @vite('resources/css/style.css')
+    <style>
+        .bg-slideshow {
+            position: fixed;
+            width: 100vw;
+            height: 100vh;
+            top: 0;
+            left: 0;
+            z-index: -1;
+            background-color: #f0f2f5; 
+        }
+        .bg-slideshow .bg-slide {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-size: cover;
+            background-position: center;
+            opacity: 0;
+            animation: slideFade 15s infinite;
+        }
+        .bg-slideshow .bg-slide:nth-child(1) {
+            background-image: url('{{ asset("images/professional_bg_1.png") }}');
+            animation-delay: 0s;
+        }
+        .bg-slideshow .bg-slide:nth-child(2) {
+            background-image: url('{{ asset("images/professional_bg_2.png") }}');
+            animation-delay: 5s;
+        }
+        .bg-slideshow .bg-slide:nth-child(3) {
+            background-image: url('{{ asset("images/professional_bg_3.png") }}');
+            animation-delay: 10s;
+        }
+
+        @keyframes slideFade {
+            0% { opacity: 0; transform: scale(1); }
+            10% { opacity: 1; }
+            33% { opacity: 1; }
+            43% { opacity: 0; }
+            100% { opacity: 0; transform: scale(1.05); }
+        }
+        
+        body.auth-page-body {
+            background: transparent !important;
+        }
+    </style>
 </head>
 <body class="auth-page-body">
+    <div class="bg-slideshow">
+        <div class="bg-slide"></div>
+        <div class="bg-slide"></div>
+        <div class="bg-slide"></div>
+    </div>
     <div class="auth-container">
         <form method="POST" action="{{ url('/login') }}">
             @csrf
